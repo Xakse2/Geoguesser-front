50a1f45b22ffa04e2009428a977f507d3b8b1641
Xakse Xakse <roma.roma.romanov2001@gmail.com>
2025-12-11 18:36:03 +0300

    test10

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,15 +2,44 @@ import js from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
 import { defineConfig } from 'eslint/config';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
 export default defineConfig([
-  {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    plugins: { js },
-    extends: ['js/recommended'],
-    languageOptions: { globals: globals.browser },
-  },
-  tseslint.configs.recommended,
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettierConfig,
   pluginReact.configs.flat.recommended,
+  pluginReactHooks.configs.flat.recommended,
+  {
+    files: ['src/**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tseslint.parser,
+      globals: globals.browser,
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'no-console': 'warn',
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      'prettier/prettier': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'warn',
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-no-target-blank': 'warn',
+      'react/prop-types': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
 ]);
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from './App.tsx';
 console.log('123');
 console.log('123');
 console.log('123');
+console.log('123');
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
